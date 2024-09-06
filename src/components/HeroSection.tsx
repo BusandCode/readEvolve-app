ba5f732f6bc4e079faea3406fb7879c8e264d3a0
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2024-09-06 15:07:44 +0100

    refactor: update tagline styles

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-gray-900 mb-6">
             Ready to Step Up Your Personal Development Game?
           </h1>
-          <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-8 w-3/4">
+          <p className="text-lg sm:text-xl lg:text-2xl text-gray-700 mb-8">
             Transform your mindset, become a growth-oriented individual and an
             intentional reader with our intensive program.
           </p>
@@ -17,7 +17,7 @@ export default function HeroSection() {
             href="#learn-more"
             className="bg-[#25D167] text-white px-8 py-4 rounded-lg shadow-lg hover:opacity-80 transition duration-300"
           >
-            Learn More
+            Get Started
           </a>
         </div>
         {/* Image */}
