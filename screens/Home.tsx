3fc4b9a9e6debf2566020a4bff11728cacfac656
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-18 21:14:57 +0100

    key features

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import book from "../src/assets/book.jpg";
+import bookj from "../src/assets/book.png";
 import check from "../src/assets/check.png"
 import book2 from "../src/assets/book2.jpg";
 import book3 from "../src/assets/book3.jpg";
@@ -198,8 +199,8 @@ export default function Home() {
             {/* Card 1 */}
             <VStack className="bg-white p-4 h-60 rounded-lg shadow-lg flex flex-col items-start justify-center">
               <div className="p-4">
-                <h2 className="text-[17px] lg:text-xl font-semibold text-gray-900 mb-6">
-                  📌 CURATED BOOK LISTS
+                <h2 className="flex items-center gap-2  lg:text-xl font-semibold text-gray-900 mb-6">
+                  <img src={bookj} alt="book" width={50}/> <small className="text-[17px]">CURATED BOOK LISTS</small>
                 </h2>
                 <p className="text-gray-700">
                   Carefully curated books that inspire growth.
