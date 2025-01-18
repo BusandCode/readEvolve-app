1965ad035be6c5cfce7d0fc5e837d681fbf04ec5
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-01-18 21:44:44 +0100

    what we do

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import book from "../src/assets/book.jpg";
+import event from "../src/assets/event.png";
 import bookj from "../src/assets/book.png";
 import check from "../src/assets/check.png"
 import book2 from "../src/assets/book2.jpg";
@@ -200,7 +201,7 @@ export default function Home() {
             <VStack className="bg-white p-4 h-60 rounded-lg shadow-lg flex flex-col items-start justify-center">
               <div className="p-4">
                 <h2 className="flex items-center gap-2  lg:text-xl font-semibold text-gray-900 mb-6">
-                  <img src={bookj} alt="book" width={50}/> <small className="text-[17px]">CURATED BOOK LISTS</small>
+                  <img src={bookj} alt="book" width={60}/> <small className="text-[17px]">CURATED BOOK LISTS</small>
                 </h2>
                 <p className="text-gray-700">
                   Carefully curated books that inspire growth.
@@ -212,8 +213,9 @@ export default function Home() {
             {/* Card 2 */}
             <VStack className="bg-white h-60 p-4 rounded-lg shadow-lg flex flex-col items-start justify-center">
               <div className="p-4">
-                <h2 className="text-[17px] lg:text-xl  font-semibold text-gray-900 mb-4">
-                  📌 INTERACTIVE EVENTS
+                <h2 className="flex items-center gap-2 lg:text-xl  font-semibold text-gray-900 mb-4">
+                  <img src={event} alt="event-image" width={50}/> 
+                  <small className="text-[17px]">INTERACTIVE EVENTS</small>
                 </h2>
                 <p className="text-gray-700">
                   Webinars, discussions, and workshops and exclusive physical
